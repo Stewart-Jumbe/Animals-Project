398c152143f37b53b7ecf1569cba24203cbd8a41
Stewart <stewartjumbe@gmail.com>
2022-01-24 15:48:09 +0000

    updated POM with Sonar dependencies and created unit test

--- src/test/java/com/tsi/sjumbe/animals/testing/AnimalTest.java
+++ src/test/java/com/tsi/sjumbe/animals/testing/AnimalTest.java
@@ -8,9 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AnimalTest
 
 {
-  @Test
-    public void test_set_name(){
+    @Test
+    void test_set_name(){
         Animal testAnimal = new Animal();
         assertEquals(testAnimal.setName("TestName"),"TestName","The name of the animal does not match what you set it as");
     }
+
+
 }
